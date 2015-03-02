8c0d6446a7a9a6617a8b15cbf2a6445d9732a7f2
Nayana Hettiarachchi <nayana@corp-gems.com>
2015-03-02 20:34:53 +0700

    Update index.js

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,7 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function() {
-  var h1 = document.getElementsByTagName('h1');
-  if (h1.length > 0) {
-    h1[0].innerText = h1[0].innerText + ' \'Allo';
+  var browser = document.getElementById("hidIsCompatible");
+  if (browser != undefined) {
+    browser.value = 'yes';
   }
 }, false);
